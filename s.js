3f2755ed65a38a9a151c0f4e68d6aa78c5c453b6
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-03-25 21:41:24 +0500

    Update s.js

--- s.js
+++ s.js
@@ -1,48 +1,60 @@
 (function () {
     'use strict';
 
+    function showNotice(msg) {
+        Lampa.Noty.show(msg);
+    }
+
     function start() {
-        // Регистрируем компонент ПЕРЕД добавлением в меню
-        Lampa.Component.add('zonafilm', function (object) {
-            var comp = this;
-            var html = $('<div class="about"><div class="about__text">Плагин работает!</div></div>');
+        showNotice('ZonaFilm: start() вызван');
 
-            this.create = function () {
-                comp.activity.loader(false);
-                comp.activity.toggle();
-                return html;
-            };
+        try {
+            Lampa.Component.add('zonafilm', function (object) {
+                var comp = this;
+                var html = $('<div class="about"><div class="about__text">Плагин ZonaFilm работает!</div></div>');
 
-            this.back = function () {
-                Lampa.Activity.backward();
-            };
+                this.create = function () {
+                    comp.activity.loader(false);
+                    comp.activity.toggle();
+                    return html;
+                };
 
-            this.destroy = function () {
-                html.remove();
-            };
-        });
+                this.back = function () {
+                    Lampa.Activity.backward();
+                };
 
-        // Добавляем пункт в меню
-        Lampa.Menu.add({
-            title: 'ZonaFilm',
-            subtitle: '',
-            icon: 'folder',
-            action: function () {
+                this.destroy = function () {
+                    html.remove();
+                };
+            });
+
+            showNotice('ZonaFilm: компонент зарегистрирован');
+
+            var icon = $('<li class="menu__item selector" data-action="zonafilm"><div class="menu__ico"><svg viewBox="0 0 24 24"><path d="M10 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg></div><div class="menu__text">ZonaFilm</div></li>');
+
+            var menu = $('.menu .menu__list');
+            showNotice('ZonaFilm: меню найдено — ' + menu.length);
+
+            menu.append(icon);
+
+            icon.on('click', function () {
                 Lampa.Activity.push({
                     url: '',
                     title: 'ZonaFilm',
                     component: 'zonafilm',
                     page: 1
                 });
-            }
-        });
+            });
+
+            showNotice('ZonaFilm: иконка добавлена');
+
+        } catch (e) {
+            showNotice('ZonaFilm ОШИБКА: ' + e.message);
+        }
     }
 
-    // Ждём готовности приложения
-    if (window.Lampa) {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') start();
-        });
-    }
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type === 'ready') start();
+    });
 
 })();
