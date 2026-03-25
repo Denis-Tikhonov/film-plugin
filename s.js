7cd823fba88bdb37e91d2d3787df3c0fd17df833
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-03-25 22:15:23 +0500

    Update s.js

--- s.js
+++ s.js
@@ -3,7 +3,7 @@
 
     Lampa.Listener.follow('app', function (e) {
         if (e.type === 'ready') {
-
+            // Регистрируем компонент
             Lampa.Component.add('zonafilm', function (object) {
                 var comp = this;
                 var html = $('<div style="padding:20px;overflow-y:auto"><div id="zf-status">Загрузка...</div></div>');
@@ -12,7 +12,7 @@
                     Lampa.Noty.show('create() вызван');
                     comp.activity.loader(true);
 
-                    // Тест через allorigins
+                    // Запрос через allorigins
                     $.get('https://api.allorigins.win/get?url=' + encodeURIComponent('https://zonafilm.ru/movies'))
                         .done(function (data) {
                             Lampa.Noty.show('Данные получены: ' + data.contents.length + ' символов');
@@ -39,20 +39,20 @@
                 };
             });
 
-            setTimeout(function () {
-                var icon = $('<li class="menu__item selector"><div class="menu__ico"><svg viewBox="0 0 24 24"><path d="M10 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg></div><div class="menu__text">ZonaFilm</div></li>');
-                $('.menu .menu__list').append(icon);
-                icon.on('click', function () {
-                    Lampa.Noty.show('Клик по меню!');
-                    Lampa.Activity.push({
-                        url: '',
-                        title: 'ZonaFilm',
-                        component: 'zonafilm',
-                        page: 1
-                    });
+            // Добавление пункта меню
+            var icon = $('<li class="menu__item selector"><div class="menu__ico"><svg viewBox="0 0 24 24"><path d="M10 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg></div><div class="menu__text">ZonaFilm</div></li>');
+            $('.menu .menu__list').append(icon);
+
+            // Используем стрелочную функцию, чтобы сохранить контекст
+            icon.on('click', () => {
+                Lampa.Noty.show('Клик по меню!');
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'ZonaFilm',
+                    component: 'zonafilm',
+                    page: 1
                 });
-            }, 2000);
+            });
         }
     });
-
 })();
