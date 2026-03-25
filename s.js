0afd88757b32ea27bdded3655c799571f60c74dc
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-03-25 21:27:33 +0500

    Update s.js

--- s.js
+++ s.js
@@ -1,1 +1,31 @@
-alert('PLUGIN LOADED');
+(function () {
+    'use strict';
+
+    function start() {
+        // Добавляем пункт в левое меню
+        Lampa.Panel.add({
+            title: 'ZonaFilm',
+            component: 'zonafilm',
+            icon: 'folder'
+        });
+
+        // Регистрируем компонент
+        Lampa.Component.add('zonafilm', function () {
+            let html = $('<div class="about"><div class="about__text">Плагин работает!</div></div>');
+            Lampa.Controller.add('zonafilm', {
+                toggle: function () {
+                    Lampa.Controller.collectionSet(html);
+                    Lampa.Controller.collectionFocus(false, html);
+                },
+                back: function () {
+                    Lampa.Controller.toggle('menu');
+                }
+            });
+            Lampa.Controller.toggle('zonafilm');
+        });
+    }
+
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type === 'ready') start();
+    });
+})();
