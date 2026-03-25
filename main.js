fc300446090e774626c0e98c0794bfdd55d61625
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-03-25 21:08:19 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,26 @@
     'use strict';
 
     function start() {
+        // Добавляем пункт в левое меню
         Lampa.Panel.add({
             title: 'ZonaFilm',
             component: 'zonafilm',
             icon: 'folder'
         });
 
+        // Регистрируем компонент
         Lampa.Component.add('zonafilm', function () {
-            Lampa.Noty.show('Плагин работает!');
+            let html = $('<div class="about"><div class="about__text">Плагин работает!</div></div>');
+            Lampa.Controller.add('zonafilm', {
+                toggle: function () {
+                    Lampa.Controller.collectionSet(html);
+                    Lampa.Controller.collectionFocus(false, html);
+                },
+                back: function () {
+                    Lampa.Controller.toggle('menu');
+                }
+            });
+            Lampa.Controller.toggle('zonafilm');
         });
     }
 
