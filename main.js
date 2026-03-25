92174697244b1628568b4fa27b00aa1685fbc2db
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-03-25 21:03:16 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,85 +2,18 @@
     'use strict';
 
     function start() {
-        if (!window.Lampa) return;
-
-        // Добавляем пункт в главное меню
-        Lampa.Menu.add({
+        Lampa.Panel.add({
             title: 'ZonaFilm',
-            icon: 'folder',
-            component: 'zonafilm'
+            component: 'zonafilm',
+            icon: 'folder'
         });
 
-        // Регистрируем компонент
-        Lampa.Component.add('zonafilm', openZonaFilm);
-    }
-
-    function openZonaFilm() {
-        let scroll = new Lampa.Scroll({ mask: true });
-        let body = $('<div class="zonafilm"></div>');
-
-        Lampa.Utils.request({
-            url: 'https://zonafilm.ru/',
-            method: 'GET',
-            dataType: 'text'
-        }, (html) => {
-            let dom = $(html);
-
-            dom.find('.shortstory').each(function () {
-                let item = $(this);
-                let title = item.find('.zagolovki h2').text().trim();
-                let link = item.find('a').attr('href');
-                let poster = item.find('img').attr('src');
-
-                let card = Lampa.Template.get('card', {
-                    title: title,
-                    poster: poster
-                });
-
-                card.on('hover:enter', () => openMovie(link));
-
-                body.append(card);
-            });
-
-            scroll.append(body);
-
-            Lampa.Controller.add('zonafilm', {
-                toggle: function () {
-                    Lampa.Controller.collectionSet(scroll.render(), body);
-                    Lampa.Controller.collectionFocus(false, scroll.render());
-                },
-                back: function () {
-                    Lampa.Controller.toggle('menu');
-                }
-            });
-
-            Lampa.Controller.toggle('zonafilm');
+        Lampa.Component.add('zonafilm', function () {
+            Lampa.Noty.show('Плагин работает!');
         });
     }
 
-    function openMovie(url) {
-        Lampa.Utils.request({
-            url: url,
-            method: 'GET',
-            dataType: 'text'
-        }, (html) => {
-            let dom = $(html);
-
-            let iframe = dom.find('iframe').attr('src');
-
-            if (iframe) {
-                Lampa.Player.play({
-                    title: 'Просмотр',
-                    url: iframe
-                });
-            } else {
-                Lampa.Noty.show('Видео не найдено');
-            }
-        });
-    }
-
-    if (window.appready) start();
-    else Lampa.Listener.follow('app', function (e) {
+    Lampa.Listener.follow('app', function (e) {
         if (e.type === 'ready') start();
     });
 })();
